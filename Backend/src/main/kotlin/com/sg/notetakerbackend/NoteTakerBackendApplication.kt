ea5d57f40f56d785a00effcf622cb31bacc60b9e
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-12-17 23:20:45 +0530

    feat: add backend files

---
+++ Backend/src/main/kotlin/com/sg/notetakerbackend/NoteTakerBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.sg.notetakerbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NoteTakerBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<NoteTakerBackendApplication>(*args)
+}
